5641e1e6a62cb9fe80d6420df3904d928e652b4b
Stefan Werfling <stefan.werfling@pegenau.de>
2022-05-22 12:46:11 +0200

    update basic auth, add Credential, update ui

--- src/Content/Form/SelectBottemBorderOnly2.ts
+++ src/Content/Form/SelectBottemBorderOnly2.ts
@@ -18,24 +18,51 @@ export type SelectChangeFn = (value: any) => void;
  */
 export class SelectBottemBorderOnly2 extends Element {
 
+    /**
+     * selected value
+     * @protected
+     */
     protected _selectedValue: string = '';
 
+    /**
+     * constructor
+     * @param element
+     */
     public constructor(element: any) {
         super();
 
         this._element = jQuery('<select class="custom-select form-control-border border-width-2" />').appendTo(element);
     }
 
+    /**
+     * setValues
+     * @param options
+     */
     public setValues(options: SelectOption[]): void {
         for (const aoption of options) {
             this.addValue(aoption);
         }
     }
 
+    /**
+     * addValue
+     * @param option
+     */
     public addValue(option: SelectOption): void {
         jQuery(`<option value="${option.key}">${option.value}</option>`).appendTo(this._element);
     }
 
+    /**
+     * clearValues
+     */
+    public clearValues(): void {
+        this._element.empty();
+    }
+
+    /**
+     * setChangeFn
+     * @param onChangeFn
+     */
     public setChangeFn(onChangeFn: SelectChangeFn): void {
         this._element.unbind().on('change', (): void => {
             this._selectedValue = this._element.val();
@@ -43,11 +70,18 @@ export class SelectBottemBorderOnly2 extends Element {
         });
     }
 
+    /**
+     * setSelectedValue
+     * @param value
+     */
     public setSelectedValue(value: string): void {
         this._selectedValue = value;
         this._element.val(value).change();
     }
 
+    /**
+     * getSelectedValue
+     */
     public getSelectedValue(): string {
         return this._selectedValue;
     }
