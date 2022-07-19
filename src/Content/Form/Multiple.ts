1a43a5a0869de7a89e539f9ce47c1e443b018a0d
Stefan Werfling <stefan.werfling@pegenau.de>
2022-07-20 00:01:30 +0200

    add dyndns client list/ui, move class, update provider/db, add multiple ui object, update frontend page

---
+++ src/Content/Form/Multiple.ts
@@ -0,0 +1,54 @@
+import {Element} from '../../Element';
+import {SelectOption} from './SelectBottemBorderOnly2';
+
+/**
+ * Multiple
+ */
+export class Multiple extends Element {
+
+    /**
+     * constructor
+     * @param element
+     * @param id
+     */
+    public constructor(element: any, id?: string) {
+        super();
+
+        const telement = this._getAnyElement(element);
+
+        let aid: string = '';
+
+        if (id) {
+            aid = id;
+        }
+
+        this._element = jQuery(`<select class="select2bs4" id="${aid}" multiple="multiple" data-placeholder="Select a State" style="width: 100%;" />`).appendTo(telement);
+    }
+
+    /**
+     * _reload
+     * @protected
+     */
+    protected _reload(): void {
+        this._element.select2({
+            theme: 'bootstrap4'
+        });
+    }
+
+    /**
+     * addValue
+     * @param option
+     */
+    public addValue(option: SelectOption): void {
+        jQuery(`<option value="${option.key}">${option.value}</option>`).appendTo(this._element);
+        this._reload();
+    }
+
+    /**
+     * clearValues
+     */
+    public clearValues(): void {
+        this._element.empty();
+        this._reload();
+    }
+}
