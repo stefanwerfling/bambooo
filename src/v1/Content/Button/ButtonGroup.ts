81abb9ed00f1867c6a2c377767128b31a6a85b64
Stefan Werfling <stefan.werfling@googlemail.com>
2024-10-01 13:48:25 +0200

    update button group add class

--- src/v1/Content/Button/ButtonGroup.ts
+++ src/v1/Content/Button/ButtonGroup.ts
@@ -7,13 +7,20 @@ export class ButtonGroup extends Element {
 
     /**
      * constructor
-     * @param element
+     * @param {any|Element} element
+     * @param {string} addClass
      */
-    public constructor(element: any) {
+    public constructor(element: any, addClass?: string) {
         super();
 
+        let tclass = '';
+
+        if (addClass) {
+            tclass = `${tclass} ${addClass}`;
+        }
+
         this._element = jQuery(
-            '<div class="btn-group" />'
+            `<div class="btn-group ${tclass}" />`
         ).appendTo(
             this._getAnyElement(element)
         );
