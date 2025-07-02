05f3a83e0475b01e1232cc24d1b0576317cda5a0
Stefan Werfling <stefan.werfling@googlemail.com>
2025-07-02 09:54:52 +0200

    update package

--- dist/v1/Content/Form/FileUpload.d.ts
+++
@@ -1,4 +0,0 @@
-import { Card, CardBodyType, CardLine, CardType } from '../Card/Card';
-export declare class FileUpload extends Card {
-    constructor(elementObject: any, bodyType?: CardBodyType, cardType?: CardType, cardLine?: CardLine);
-}
--- dist/v1/Navbar/LeftNavbarPushmenu.d.ts
+++
@@ -1,6 +0,0 @@
-import { LeftNavbar } from './LeftNavbar';
-export declare class LeftNavbarPushmenu {
-    private _li;
-    constructor(leftNavbar: LeftNavbar);
-    getLiElement(): any;
-}
